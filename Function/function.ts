f86c0383e036ba2be4dd37c3ad0df235da6ae37a
iqbaladudu <iqbal.adudu@gmail.com>
2021-10-03 20:44:46 +0200

    FIX: Linting error in Ts file

--- Function/function.ts
+++ Function/function.ts
@@ -1,49 +1,45 @@
-namespace Fungsi {
-    /*
+/*
 
-    Di dalam Typescript, kita bisa menambahkan anotasi tipe data di dalam parameter seperti: String, Number, Array, dan lainnya.
+Di dalam Typescript, kita bisa menambahkan anotasi tipe data di dalam parameter seperti: String, Number, Array, dan lainnya.
 
-    **Tetap fokus di kode yang ada di dalam keyword namespace**
+**Tetap fokus di kode yang ada di dalam keyword namespace**
 
-    */
-    function nyapa(waktu: number, nama: string, ucapanSelamat: string[]) {
-        if (waktu >= 5 && waktu <= 12) {
-            console.log(`Halo ${nama}, ${ucapanSelamat[0]}`)
-        } else if (waktu >= 13 && waktu <= 15) {
-            console.log(`Halo ${nama}, ${ucapanSelamat[1]}`)
-        } else if (waktu >= 16 && waktu <= 4) {
-            console.log(`Halo ${nama}, ${ucapanSelamat[2]}`)
-        }
+*/
+function nyapa(waktu: number, nama: string, ucapanSelamat: string[]) {
+    if (waktu >= 5 && waktu <= 12) {
+        console.log(`Halo ${nama}, ${ucapanSelamat[0]}`)
+    } else if (waktu >= 13 && waktu <= 15) {
+        console.log(`Halo ${nama}, ${ucapanSelamat[1]}`)
+    } else if (waktu >= 16 && waktu <= 4) {
+        console.log(`Halo ${nama}, ${ucapanSelamat[2]}`)
     }
+}
     
-    const ucapanSelamat: string[] = [
-        "Selamat Pagi",
-        "Selamat Siang",
-        "Selamat Malam"
-    ]
+const ucapanSelamat: string[] = [
+    "Selamat Pagi",
+    "Selamat Siang",
+    "Selamat Malam"
+];
     
+/* 
+    
+Parameter yang dimasukkan hatus memiliki tipe data yang sama dengan argumen fungsi di atas 
+    
+*/
+nyapa(12, "Iqbal", ucapanSelamat);
+
+
+/*
+    
+Di dalam Typescript, kita bisa menentukan tipe data tertentu untuk nilai kembalian(return) fungsi, fitur ini juga dikenal dengan Return Type Annotations, anotasi tipe data tersebut bisa kita tambahkan setelah parameter ketika menulis fungsi
+function name(): <tulis-di-sini> {}
+    
+*/
+function CapitalizedName(name: string): string {
     /* 
-    
-    Parameter yang dimasukkan hatus memiliki tipe data yang sama dengan argumen fungsi di atas 
-    
+    Nilai kembaliannya harus string juga. Jika tidak maka akan terjadi error
     */
-    nyapa(12, "Iqbal", ucapanSelamat)
+    return name.toUpperCase();
 }
 
-namespace returnTypeAnnotation {
-    /*
-    
-    Di dalam Typescript, kita bisa menentukan tipe data tertentu untuk nilai kembalian(return) fungsi, fitur ini juga dikenal dengan Return Type Annotations, anotasi tipe data tersebut bisa kita tambahkan setelah parameter ketika menulis fungsi
-    function name(): <tulis-di-sini> {}
-    
-    */
-    function CapitalizedName(name: any): string {
-        /* 
-        Nilai kembaliannya harus string juga. Jika tidak maka akan terjadi error
-        */
-        return name.toUpperCase()
-    }
-
-    console.log(CapitalizedName("iqbal"))
-
-}
+console.log(CapitalizedName("iqbal"));
