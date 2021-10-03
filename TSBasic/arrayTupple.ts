638b42d5b17bf9e7fa757be18827b67483dcff07
ezza022 <alfirman.pahlepi@gmail.com>
2021-10-03 21:08:28 +0700

    add: menambahkan file arrayTupple.ts

---
+++ TSBasic/arrayTupple.ts
@@ -0,0 +1,51 @@
+/**
+ * Array
+ * 1. Array of Number
+ * 2. Array of String
+ * 3. Array with Union
+ * 4. Tuple
+ * 5. Array of Object
+ * 6. Array multi dimension
+ * 7. Destructuring
+ */
+
+/**
+ * 1. Array of Number
+ * [1,2,3,4]
+ */
+
+/**
+ * 2. Array of String
+ * ['a', 'b', 'c']
+ */
+
+/**
+ * 3. Array with Union
+ * ["hello", 1, 2, 3 ]
+ */
+
+/**
+ * 4. Tuple
+ * ['cordinat', 2, 4, 7]
+ */
+
+/**
+ * 5. Array of Object
+ * [
+ *   { color: 'blue', index: 1 },
+ *   { color: 'red', index: 2 },
+ * ]
+ */
+
+/**
+ * 6. Array multi dimension [[]]
+ * matrix = [
+ *  [1, 2],
+ *  [3, 4],
+ * ];
+ */
+
+/**
+ * 7. Destructuring
+ * let [a, b, c, d]: [number, number, number, string] = [1, 2, 3, 'four'];
+ */
