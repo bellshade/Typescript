30e32a218d03763b7b172b1fbea2f0afaab336cb
rakhmadi <rakhmadiwalker@gmail.com>
2021-10-04 19:27:49 +0700

    fix: update comment squareEveryDigit

--- TSExample/squareEveryDigits.ts
+++ TSExample/squareEveryDigits.ts
@@ -1,6 +1,9 @@
-// mengkuadratkan setiap digit angka dan menggabungkannya.
-// contoh input 6162 
-// output : 6^2,1^2,6^2,2^2 => '36','1','36','4' => 361364
+/**
+  mengkuadratkan setiap digit angka dan menggabungkannya.
+  contoh input 6162 
+  output : 6^2,1^2,6^2,2^2 => '36','1','36','4' => 361364
+ */
+
   function pangkatkanPerDigit(num: number):number {
     const arrayNum:string = Array.from(String(num),Number).map((value)=>{
       return Math.pow(value,2)
