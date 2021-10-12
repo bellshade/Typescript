7fec05e0fcacebd64b7c1d9d930eb8711a0c6f63
aldhanekaa <aldhanekadev@gmail.com>
2021-10-12 20:09:42 +0700

    docs: mengenai Object-Types dan interface

---
+++ TSBasic/Object-Types/interface.ts
@@ -0,0 +1,88 @@
+/*
+ * Interface adalah Object Types hanya untuk menuntukan tipe data object.
+ * seperti variable object, function parameter yang membutuhkan object, dan function return yang mengembalikan object.
+ */
+
+/*
+ * Syntax, cara penggunaan keyword interface
+
+    interface <nama interface> {
+        [key object]?: <tipe data> 
+    }
+
+ * interface merupakan keyword
+ * <nama interface>, nama interface mu
+ * [key object] adalah nama key pada object 
+ * ?, tanda tanya ini digunakan ketika key pada object mempunyai kemungkinan tidak bernilai atau undefined
+ * <tipe data>, tipe data bisa berupa tipe data dasar builtin Typescript (string, number, boolean) bisa juga tipe data interface atau type
+
+ */
+
+interface Pelajar { // interface dengan nama Pelajar
+    nama: string; // pada interface ini, dan object yang memiliki tipe data interface Pelajar akan mempunyai key object nama dengan tipe data string
+    umur: number; // key object umur dengan tipe data number (nomor)
+    sudahDiVaksin?: boolean; // key object dengan nama sudahDiVaksin dengan tipe data boolean, dan menggunakan `?` (tanda tanya) yang berarti ada kemungkinan data ini bukan boolean melainkan undefined
+}
+
+let Udin: Pelajar = {
+    nama: "Udin",
+    umur: 19
+    // key object sudahDiVaksin tidak ada dan tidak terjadi error disini tampaknya teman-teman!
+}
+
+let Udon: Pelajar = {
+    nama: "Udon",
+    /* 
+    key object umur tidak ada dan terjadi error:
+    "Property 'umur' is missing in type '{ nama: string; }' but required in type 'Pelajar'."
+    yang artinya : "Properti 'umur' tidak ada dalam tipe '{ nama: string; }' tetapi diperlukan dalam jenis 'Pelajar'."
+
+    */
+
+    // key object sudahDiVaksin tidak ada, dan tidak terjadi error disini tampaknya teman-teman!
+}
+
+// contoh pengaplikasian lainnya adalah menggunakan interface pada parameter function atau nilai yang dikembalikan function
+
+interface ParameterFunctionPertambahanAdanB { // kita mendefinisikan interface dengan nama ParameterFunctionPertambahanAdanB
+    a: number; // pada interface ini artinya kita akan mempunyai object dengan key a dengan tipe data number
+    b: number; // pada interface ini artinya kita akan mempunyai object dengan key b dengan tipe data number
+}
+
+function PertambahanAdanB(input: ParameterFunctionPertambahanAdanB): number {  // mendefinisikan fungsi PertambahanAdanB yang mengembalikan data number, dengan parameter input yang merupakan object dan memiliki tipe data interface ParameterFunctionPertambahanAdanB
+    return input.a + input.b
+}
+/* 
+    Dengan mendefinisikan parameter input kepada interface ParameterFunctionPertambahanAdanB, itu sama saja dengan kode yang dibawah ini 👇👇
+    function PertambahanAdanB(input: {a: number; b: number}): number {
+    }
+*/
+
+
+/*
+    keyword `extends` adalah salah satu keyword lainnya yang dapat digunakan untuk interface.
+    keyword extends digunakan untuk memiliki tipe data interface yang sama.
+
+    Pada kode dibawah 👇👇
+    kita mendefinisikan interface dengan nama registeredUser
+    kita extends interface Pelajar kepada interface registeredUser, dengan demikian konten yang dimiliki interface Pelajar akan juga dimiliki oleh interface registeredUser.
+
+*/
+interface registeredUser extends Pelajar{
+    dibuatPada: number; // pada interface ini artinya kita akan mempunyai object dengan key dibuatPada dengan tipe data number 
+}
+
+function RegisterUser(user: Pelajar): registeredUser {  // mendefinisikan fungsi PertambahanAdanB yang mengembalikan data number, dengan parameter input yang merupakan object dan memiliki tipe data interface ParameterFunctionPertambahanAdanB
+    
+    // return user <- jika kita hanya return user akan terjadi error karena tipe data user adalah interface Pelajar dan tidak memiliki object key dibuatPada
+
+    return { // return berhasil karena tipe data yang dikembalikan function RegisterUser mengikuti tipe data registeredUser
+        ...user,
+        dibuatPada: Date.now(),
+        /*
+            tipe data yang kita definisikan pada pengembalian function sangat sensitif terhadap nilai yang dikembalikan, jika nilai yang dikembalikan tidak mengikuti tipe data pengembalian function maka akan terjadi error
+
+            kunciMisterius: "ABC" <- jika kita uncomment ini dan menaruhnya pada object return maka akan terjadi error karena key `kunciMisterius` tidak ada dalam definisi interface registeredUser
+        */
+    }
+}
---
+++ TSBasic/Object-Types/readme.md
@@ -0,0 +1,16 @@
+# Object Types pada Typescript
+Object Types sendiri adalah tipe data untuk menentukan tipe data pada Variable, Function, Object, dan Array. Object Types terdiri dari 2 bagian yaitu `interface` dan `type`
+
+## interface 
+interface _keyword_, dan ia adalah Object Types. Keyword ini digunakan **hanya** untuk menuntukan tipe data object, seperti variable object, function parameter yang membutuhkan object, dan function return yang mereturn object.
+
+tolong lihat `interface.ts` untuk melihat contoh pengaplikasiannya.
+
+## type 
+`type` sendiri adalah _keyword_ pada Typescript, dan ia adalah Object Types. Keyword ini dapat digunakan untuk menentukan tipe data variable, tipe data function, dan tipe data object.
+
+tolong lihat `type.ts` untuk melihat contoh pengaplikasiannya.
+
+## Perbedaan interface dan Type
+Perbedaan dari keduanya ialah `interface` digunakan **hanya** untuk menuntukan tipe data object, seperti variable object. Sedangkan `type` digunakan untuk menentukan tipe data variable, tipe data function, dan tipe data object. 
+
