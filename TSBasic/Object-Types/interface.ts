992ca24cd2ff3e643bc3e1465db514b5922b7b06
aldhanekaa <aldhanekadev@gmail.com>
2021-10-25 10:26:29 +0700

    docs: fix eslint error on interface.ts

--- TSBasic/Object-Types/interface.ts
+++ TSBasic/Object-Types/interface.ts
@@ -1,18 +1,18 @@
-/*
+/**
  * Interface adalah Object Types hanya untuk menuntukan tipe data object.
  * seperti variable object, function parameter yang membutuhkan object, dan function return yang mengembalikan object.
  */
 
-/*
+/**
  * Syntax, cara penggunaan keyword interface
 
     interface <nama interface> {
-        [key object]?: <tipe data> 
+        [key object]?: <tipe data>
     }
 
  * interface merupakan keyword
  * <nama interface>, nama interface mu
- * [key object] adalah nama key pada object 
+ * [key object] adalah nama key pada object
  * ?, tanda tanya ini digunakan ketika key pada object mempunyai kemungkinan tidak bernilai atau undefined
  * <tipe data>, tipe data bisa berupa tipe data dasar builtin Typescript (string, number, boolean) bisa juga tipe data interface atau type
 
@@ -22,35 +22,37 @@ interface Pelajar { // interface dengan nama Pelajar
     nama: string; // pada interface ini, dan object yang memiliki tipe data interface Pelajar akan mempunyai key object nama dengan tipe data string
     umur: number; // key object umur dengan tipe data number (nomor)
     sudahDiVaksin?: boolean; // key object dengan nama sudahDiVaksin dengan tipe data boolean, dan menggunakan `?` (tanda tanya) yang berarti ada kemungkinan data ini bukan boolean melainkan undefined
-    
+
     // kita juga dapat mendefinisikan fungsi dalam interface jika pada variable object membutuhkan sebuah fungsi!
     getNamaPelajar(): string; // artinya pada object yang akan di definisikan tipe datanya pada interface Pelajar akan memiliki fungsi bernama getNamaPelajar yang mengeluarkan tipe data string
 
 }
 
 const Udin: Pelajar = {
-    nama: "Udin",
+    nama: 'Udin',
     umur: 19,
     getNamaPelajar: function() {
-        return this.nama
+        return this.nama;
     }
     // key object sudahDiVaksin tidak ada dan tidak terjadi error disini tampaknya teman-teman!
-}
+};
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const Udon: Pelajar = {
-    nama: "Udon",
+    nama: 'Udon',
     getNamaPelajar() {
-        return this.nama 
+        return this.nama;
     }
-    /* 
+    /*
     key object umur tidak ada dan terjadi error:
     "Property 'umur' is missing in type '{ nama: string; }' but required in type 'Pelajar'."
     yang artinya : "Properti 'umur' tidak ada dalam tipe '{ nama: string; }' tetapi diperlukan dalam jenis 'Pelajar'."
 
     */
-
     // key object sudahDiVaksin tidak ada, dan tidak terjadi error disini tampaknya teman-teman!
-}
+    ,
+    umur: 0
+};
 
 // contoh pengaplikasian lainnya adalah menggunakan interface pada parameter function atau nilai yang dikembalikan function
 
@@ -60,9 +62,9 @@ interface ParameterFunctionPertambahanAdanB { // kita mendefinisikan interface d
 }
 
 function PertambahanAdanB(input: ParameterFunctionPertambahanAdanB): number {  // mendefinisikan fungsi PertambahanAdanB yang mengembalikan data number, dengan parameter input yang merupakan object dan memiliki tipe data interface ParameterFunctionPertambahanAdanB
-    return input.a + input.b
+    return input.a + input.b;
 }
-/* 
+/*
     Dengan mendefinisikan parameter input kepada interface ParameterFunctionPertambahanAdanB, itu sama saja dengan kode yang dibawah ini 👇👇
     function PertambahanAdanB(input: {a: number; b: number}): number {
     }
@@ -72,18 +74,18 @@ function PertambahanAdanB(input: ParameterFunctionPertambahanAdanB): number {  /
 /*
     keyword `extends` adalah salah satu keyword lainnya yang dapat digunakan untuk interface.
     keyword extends digunakan untuk memiliki tipe data interface yang sama.
-    
+
     Pada kode dibawah 👇👇
     kita mendefinisikan interface dengan nama registeredUser
     kita extends interface Pelajar kepada interface registeredUser, dengan demikian konten yang dimiliki interface Pelajar akan juga dimiliki oleh interface registeredUser.
 
 */
 interface registeredUser extends Pelajar{
-    dibuatPada: number; // pada interface ini artinya kita akan mempunyai object dengan key dibuatPada dengan tipe data number 
+    dibuatPada: number; // pada interface ini artinya kita akan mempunyai object dengan key dibuatPada dengan tipe data number
 }
 
 function RegisterUser(user: Pelajar): registeredUser {  // mendefinisikan fungsi PertambahanAdanB yang mengembalikan data number, dengan parameter input yang merupakan object dan memiliki tipe data interface ParameterFunctionPertambahanAdanB
-    
+
     // return user <- jika kita hanya return user akan terjadi error karena tipe data user adalah interface Pelajar dan tidak memiliki object key dibuatPada
 
     return { // return berhasil karena tipe data yang dikembalikan function RegisterUser mengikuti tipe data registeredUser
@@ -94,7 +96,7 @@ function RegisterUser(user: Pelajar): registeredUser {  // mendefinisikan fungsi
 
             kunciMisterius: "ABC" <- jika kita uncomment ini dan menaruhnya pada object return maka akan terjadi error karena key `kunciMisterius` tidak ada dalam definisi interface registeredUser
         */
-    }
+    };
 }
 
 /*
@@ -102,13 +104,13 @@ fungsi RegisterUser yang ada diatas sama saja dengan yang ada dibawah, yang memb
 Fungsi di bawah tidak menggunakan interface baru sedangkan fungsi di atas menggunakan interface baru dengan menggunakan keyword `extends`, akan tetapi fungsi bawah menggunakan `&`
 
 function RegisterUser(user: Pelajar): Pelajar & {
-    dibuatPada: number; 
-} { 
-    
-    return { 
+    dibuatPada: number;
+} {
+
+    return {
         ...user,
         dibuatPada: Date.now(),
-    
+
     }
 }
 
