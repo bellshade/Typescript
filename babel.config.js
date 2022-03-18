f7b841497f895224c6fb2e1069dd633ed603fb2f
Ikramullah <ikramullahzx@gmail.com>
2022-03-19 05:48:26 +0800

    refactor(deps): Add @babel/plugin-propoal-decorators refactor(deps): Remove yarn lock

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   presets: [
     ['@babel/preset-env', {targets: {node: 'current'}}],
+    ['@babel/plugin-proposal-decorators', {legacy: true},
     '@babel/preset-typescript',
   ],
 };
