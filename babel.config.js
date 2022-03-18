8d6d0a5933ec29cce7a50e23ca844b920efde762
Ikramullah <ikramullahzx@gmail.com>
2022-03-19 06:03:47 +0800

    Revert "refactor(deps): Add @babel/plugin-propoal-decorators"
    This reverts commit f7b841497f895224c6fb2e1069dd633ed603fb2f.

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   presets: [
     ['@babel/preset-env', {targets: {node: 'current'}}],
-    ['@babel/plugin-proposal-decorators', {legacy: true}],
     '@babel/preset-typescript',
   ],
 };
