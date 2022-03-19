dfc75c192856472a1e7e08edac2ee5ce74b35742
Mednoob <ahmadyaqdhan123@gmail.com>
2022-03-19 11:15:32 +0900

    add: menambahkan babel plugin untuk decorator dan menaruhnya ke config

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  plugins: [
+    [require('@babel/plugin-proposal-decorators').default, { legacy: true }]
+  ],
   presets: [
     ['@babel/preset-env', {targets: {node: 'current'}}],
     '@babel/preset-typescript',
